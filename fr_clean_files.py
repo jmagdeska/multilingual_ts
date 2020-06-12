91f365b51b5549b88c7e43220b1aeb0e686f552d
Jana <s261427@studenti.polito.it>
2020-06-12 13:06:34 +0200

    New french timelines added

--- fr_clean_files.py
+++ fr_clean_files.py
@@ -4,6 +4,10 @@ import json
 import dateparser
 from bs4 import BeautifulSoup
 
+months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 
+'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre']
+days = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
+
 def clean_brexit(fname):
     ind = 0
     f_out = open('fr_clean/' + fname, 'w')
@@ -76,4 +80,70 @@ def clean_ukraine2(fname):
     f_out.write('--------------------------------')
     f_out.close()
 
-clean_ukraine(sys.argv[1])
+def clean_libya(fname):
+    ind = 0
+    month_year = ''
+    f_out = open('fr_clean/' + fname, 'w')
+    with open('fr_timelines/' + fname, 'r') as fp: 
+        for line in fp:
+            content = line.split(' ')
+            if content[0].lower() in months:
+                month_year = line.strip('\n')
+            elif content[0].lower() in days:
+                full_date = content[1] + ' ' + month_year
+                curr_date = str(dateparser.parse(full_date).date()) 
+                if ind != 0: f_out.write('--------------------------------' + '\n')
+                text = '. '.join((line.split('. ')[1:]))
+                f_out.write(curr_date + '\n' + text)
+                ind += 1
+    f_out.write('--------------------------------')
+    f_out.close()          
+
+def clean_iraq(fname):
+    ind = 0
+    curr_year = ''
+    curr_month = ''
+    f_out = open('fr_clean/' + fname, 'w')
+    with open('fr_timelines/' + fname, 'r') as fp: 
+        for line in fp:
+            if line.startswith('20') and len(line.strip(' \n')) == 4:
+                curr_year = line.strip(' \n')
+                
+            elif line.strip('\n').lower() in months:
+                curr_month = line.strip('\n')
+                
+            elif line[0].isdigit():
+                day = line.split(': ')[0]
+                full_date = ''
+                if len(day.split(' ')) == 1:
+                    full_date = day + ' ' + curr_month + ' ' + curr_year
+                else:
+                    full_date = day + ' ' + curr_year
+                curr_date = str(dateparser.parse(full_date).date()) 
+                if ind != 0: f_out.write('--------------------------------' + '\n')
+                text = '. '.join((line.split(': ')[1:]))
+                f_out.write(curr_date + '\n' + text)
+                ind += 1
+            elif line!='\n': f_out.write(line)
+    f_out.write('\n' + '--------------------------------')
+    f_out.close()
+
+def clean_syria(fname):
+    ind = 0
+    f_out = open('fr_clean/' + fname, 'w')
+    with open('fr_timelines/' + fname, 'r') as fp: 
+        curr_year = ''
+        for line in fp:
+            if line[0].isdigit():
+                curr_year = line.strip('\n')
+            elif line!='\n':
+                content = line.split(' : ')
+                d = content[0].strip('- ')
+                curr_date = str(dateparser.parse(d + ' ' + curr_year).date())
+                if ind != 0 : f_out.write('--------------------------------' + '\n')
+                f_out.write(curr_date + '\n' + content[1])
+                ind += 1
+    f_out.write('\n' + '--------------------------------')
+    f_out.close()
+
+clean_syria(sys.argv[1])
