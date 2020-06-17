f8bfc64a03003d75d1a75094f1a8d577f94f6744
Jana <s261427@studenti.polito.it>
2020-06-17 22:24:44 +0200

    Added new ru timeline

--- ru_clean_files.py
+++ ru_clean_files.py
@@ -17,4 +17,23 @@ def clean(fname):
     f_out.write('\n' + '--------------------------------')
     f_out.close()
 
-clean(sys.argv[1])
+def clean_syria(fname):
+    ind = 0
+    curr_year = ''
+    f_out = open('ru_clean/' + fname, 'w')
+    with open('ru_timelines/' + fname, 'r') as fp: 
+        for line in fp:
+            content = line.split(' ')
+            if content[0] == '2011' or content[0] == '2012':
+                curr_year = content[0]
+            elif line != '\n':
+                d = content[0] + ' ' + content[1] + ' ' + curr_year
+                curr_date = str(dateparser.parse(d).date())
+                text = ' '.join(content[2:])
+                if ind != 0 : f_out.write('--------------------------------' + '\n')
+                f_out.write(curr_date + '\n' + text)
+                ind += 1
+    f_out.write('\n' + '--------------------------------')
+    f_out.close()
+
+clean_syria(sys.argv[1])
