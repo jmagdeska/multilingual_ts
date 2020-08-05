d8f6ad5e9d15fdfd5903a2a8bea9f47aa3da92b8
Jana <s261427@studenti.polito.it>
2020-08-05 22:24:24 +0200

    Corpus built for es,fr and it

--- article_preprocessing.py
+++ article_preprocessing.py
@@ -1,10 +1,13 @@
 import os
 import sys
 import spacy
-# from python-heideltime import HeidelTime
+import pickle
+from nltk import sent_tokenize
+from tilse.data import corpora
+from py_heideltime import py_heideltime
 
 lang = {'es':'es_core_news_sm', 'fr':'fr_core_news_sm', 'it':'it_core_news_sm'}
-heidel_lang = {'es': 'spanish', 'fr': 'french', 'it': 'italian'}
+heidel_lang = {'es': 'Spanish', 'fr': 'French', 'it': 'Italian', 'ru' : 'Russian'}
 
 def clean_art(l):
     for topic in os.listdir(dir_art):
@@ -22,44 +25,82 @@ def clean_art(l):
                 with open(dir_date + f, 'w+') as fp2:
                     fp2.write(f_out)
 
-def build_corpus(l):
-    nlp = spacy.load(lang[l]) 
+def sent_token(l):
+    if l != 'ru':
+        nlp = spacy.load(lang[l]) 
 
     for topic in os.listdir(dir_art):
-        if topic == 'yemen':
-            for date in os.listdir("/".join([dir_art, topic])):
-                for filename in os.listdir("/".join([dir_art, topic, date])):
-                    if '.tokenized' not in filename:
-                        print(filename)
-                        f = "/".join([dir_art, topic, date, filename])
-                        sentences = ""
-                        with open(f, 'r') as fp:
-                            for line in fp.readlines():
-                                sentences += line
-                        
-                        doc = nlp(sentences)
-                        splitted_and_tokenized = ""
+        for date in os.listdir("/".join([dir_art, topic])):
+            for filename in os.listdir("/".join([dir_art, topic, date])):
+                if '.tokenized' not in filename:
+                    f = "/".join([dir_art, topic, date, filename])
+                    sentences = ""
+                    with open(f, 'r') as fp:
+                        for line in fp.readlines():
+                            sentences += line
+                    
+                    splitted_and_tokenized = ""
 
+                    if l != 'ru':
+                        doc = nlp(sentences) 
                         for sent in doc.sents:
                             splitted_and_tokenized += " ".join(
                                 [tok.text for tok in sent if not tok.text.isspace()]).strip() + "\n"
 
-                        splitted_and_tokenized = splitted_and_tokenized.strip()
+                    else:
+                        for sent in sent_tokenize(sentences):
+                            splitted_and_tokenized += sent.strip() + "\n"
 
-                        tokenized_filename = "/".join([dir_art, topic, date,
+                    splitted_and_tokenized = splitted_and_tokenized.strip()
+                    tokenized_filename = "/".join([dir_art, topic, date,
                                                         filename]) + ".tokenized"
 
-                        with open(tokenized_filename, "w") as file:
-                            file.write(splitted_and_tokenized)
+                    with open(tokenized_filename, "w") as file:
+                        file.write(splitted_and_tokenized)
 
-# def tag_heideltime():
-#     ht = HeidelTime.HeidelTimeWrapper(heidel_lang[l])
-#     ht_sents = ''
-#     with open(dir_art + 'egypt' + '2011-01-31/14.txt.tokenized', 'r') as fp:
-#         for line in fp:
-#             heidel = ht.parse(line)
-#             print(heidel)
+def tag_heideltime(l):
+    path_corpus = l + "/corpus/"
+    if not os.path.exists(os.path.dirname(path_corpus)):
+        os.mkdir(path_corpus)
+
+    for topic in os.listdir(dir_art):
+        print("Heideltime tagging for topic:", topic)
+        topic_path = "/".join([path_corpus, topic]) + '/'
+        if not os.path.exists(os.path.dirname(topic_path)):
+            os.mkdir(topic_path)
+
+        for date in os.listdir("/".join([dir_art, topic])):
+            for filename in os.listdir("/".join([dir_art, topic, date])):
+                if '.tokenized' in filename:
+                    f = "/".join([dir_art, topic, date, filename])
+                    f_out = "/".join([topic_path, filename.split('.tokenized')[0]])
+                    text = ''
+                    fsize = os.path.getsize(f)
+                    if fsize > 0:
+                        with open(f, 'r') as fp: 
+                            for line in fp.readlines():
+                                text += line + "\n"
+
+                        res = py_heideltime(text, language=heidel_lang[l],  document_type='news')
+                        with open(f_out, "w") as file:
+                            file.write(str(res[1]))
+
+def build_corpus(l):
+    nlp = spacy.load(lang[l]) 
+    path_raw = l + '/corpus/'
+    path_dumped = l + "/dumped_corpus/"
+    if not os.path.exists(os.path.dirname(path_dumped)):
+        os.mkdir(path_dumped)
+
+    for topic in os.listdir(path_raw):
+        print("Building corpus for topic:", topic)
+        corpus = corpora.Corpus.from_folder(path_raw + topic, nlp)
+
+        with open(path_dumped + topic + ".corpus.obj", "wb") as my_file:
+            pickle.dump(corpus, my_file)
 
 l = sys.argv[1]
 dir_art = l + '/articles/'
-build_corpus(l)
+# sent_token(l)
+tag_heideltime(l)
+# build_corpus(l)
